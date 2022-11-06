0fe1294b7b3e24a4ac209f8c915921a2b4290e65
kushimoto <kusshie2@gmail.com>
2022-11-06 13:29:35 +0900

    不要なものを削除

--- electron/main.js
+++ electron/main.js
@@ -3,7 +3,6 @@ const { ipcMain } = require("electron");
 const fs = require("fs");
 const path = require("path");
 const { NodeSSH } = require('node-ssh');
-const { Client } = requier('ssh2');
 const yaml = require('js-yaml')
 
 let mainWindow;
@@ -29,7 +28,6 @@ const createWindow = () => {
 };
 
 const ssh = new NodeSSH()
-const connection = new Client()
 
 app.whenReady().then(() => {
 
